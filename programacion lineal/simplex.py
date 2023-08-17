030691238c5827ada37501d9828111dc4ea66ed7
Jose Gonzalo Scali <94322853+GonziFlowReloaded@users.noreply.github.com>
2023-08-17 21:53:45 +0000

    fist commit

---
+++ programacion lineal/simplex.py
@@ -0,0 +1,26 @@
+import cvxpy as cp
+
+# Variables de decisión
+x = cp.Variable()
+y = cp.Variable()
+
+# Función objetivo: Minimizar 2x + 3y
+objective = cp.Maximize(2*x + 4*y)
+
+# Restricciones: 
+constraints = [4*x + 6*y <= 120,
+               2*x + 6*y <= 72,
+               0*x + 1*y <= 10,
+               x >= 0,
+               y >= 0]
+
+# Definir el problema de optimización
+problem = cp.Problem(objective, constraints)
+
+# Resolver el problema
+problem.solve()
+
+print("Resultado:")
+print("Valor óptimo:", problem.value)
+print("Valor de x:", x.value)
+print("Valor de y:", y.value)
